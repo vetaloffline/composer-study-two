31e7eac8eea3a8ea10ff03fe3431073689e750d5
vetalb <1990vetalb1990@gmail.com>
2020-06-23 18:38:46 +0300

    add create categories

---
+++ src/SuperRandom2.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+class SuperRandom2
+{
+    public function getRandomString()
+    {
+        return 'KLJSHIG^#@OINSAF:JI#222222222';
+    }
+
+    public function getRandomNumber()
+    {
+        return 43622222222222;
+    }
+
+
+}
